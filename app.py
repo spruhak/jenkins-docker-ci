cf199ab3f272968e548009980aef6f657cc4b1c4
Snehal <snehal@gmail.com>
2025-12-28 15:34:38 +0000

    Fix indentation issue in feature branch

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ class App(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from Jenkins CI/CD Pipeline")
-	self.wfile.write(b"Feature branch version - testing")
+        self.wfile.write(b"Hello from Jenkins CI/CD Pipeline\n")
+        self.wfile.write(b"Feature branch version - testing")
 
 HTTPServer(("0.0.0.0", 8000), App).serve_forever()
+
